75ef71b576811925d70d3211be72ecf0174aefe5
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-05 16:40:05 -0300

    ref: separate response of riot api and my custom api

--- resources/api/v1/summoner.go
+++ resources/api/v1/summoner.go
@@ -1,12 +1,18 @@
 package v1
 
-// Summoner - gamer profile
-type Summoner struct {
+// SummonerResponse - summoner profile response
+type SummonerResponse struct {
 	ID            string `json:"id,omitempty"`
 	AccountID     string `json:"accountId,omitempty"`
 	Puuid         string `json:"puuid,omitempty"`
 	Name          string `json:"name,omitempty"`
 	ProfileIconID int    `json:"profileIconId,omitempty"`
 	RevisionDate  int    `json:"revisionDate,omitempty"`
+	SummonerLevel int    `json:"summonerLevel,omitempty"`
+}
+
+// Summoner - summoner for API
+type Summoner struct {
+	Name          string `json:"name,omitempty"`
 	SummonerLevel int    `json:"summonerLevel,omitempty"`
 }
