b05977c7b2b23e0a95e2a2f6f1849a54740d70d2
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-05 16:41:00 -0300

    ref: adjust GetByName service of summoner

--- services/api/v1/summoner.go
+++ services/api/v1/summoner.go
@@ -1,3 +1,30 @@
 package v1
 
-// responsavel por disponibilizar funções para usar manipulando o dado summoner
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	resources_v1 "github.com/antonioazambuja/ionia/resources/api/v1"
+)
+
+// GetByName - get summoner by name
+func GetByName(summonerName string, regionName string) (resources_v1.SummonerResponse, error) {
+	regions := GetAvailableRegions()
+	request, errNewRequest := http.NewRequest("GET", regions[regionName]+"/lol/summoner/v4/summoners/by-name/"+summonerName, nil)
+	if errNewRequest != nil {
+		panic(errNewRequest)
+	}
+	request.Header.Set(HeaderAPIKey, APIKey)
+	client := &http.Client{
+		Timeout: time.Duration(300 * time.Second),
+	}
+	response, errResponse := client.Do(request)
+	if errResponse != nil && response.StatusCode != 200 {
+		panic(errResponse)
+	}
+	defer response.Body.Close()
+	var summoner resources_v1.SummonerResponse
+	json.NewDecoder(response.Body).Decode(&summoner)
+	return summoner, nil
+}
