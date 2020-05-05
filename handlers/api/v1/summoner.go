83191235bd8aa8ae7435dcab36907626bf081aea
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-05 16:39:20 -0300

    ref: create handler for summoner services

--- handlers/api/v1/summoner.go
+++ handlers/api/v1/summoner.go
@@ -1,3 +1,22 @@
 package v1
 
+import (
+	"encoding/json"
+	"net/http"
+
+	svc_v1 "github.com/antonioazambuja/ionia/services/api/v1"
+	"github.com/gorilla/mux"
+)
+
 // responsavel por criar rotas e escutar requisições
+
+// GetByName - get summoner by name
+func GetByName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	summoner, err := svc_v1.GetByName(params["name"], params["region"])
+	if err != nil {
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(summoner)
+}
