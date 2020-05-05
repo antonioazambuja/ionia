940f0618ec4285b7b44353f59ffd20e4a5f44b59
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-05 16:37:18 -0300

    add: endpoints of regions available

---
+++ services/api/v1/regions.go
@@ -0,0 +1,19 @@
+package v1
+
+// GetAvailableRegions - regions of API
+func GetAvailableRegions() (regions map[string]string) {
+	regions = map[string]string{
+		"BR1":  "https://br1.api.riotgames.com",
+		"EUN1": "https://eun1.api.riotgames.com",
+		"EUW1": "https://euw1.api.riotgames.com",
+		"JP1":  "https://jp1.api.riotgames.com",
+		"KR":   "https://kr.api.riotgames.com",
+		"LA1":  "https://la1.api.riotgames.com",
+		"LA2":  "https://la2.api.riotgames.com",
+		"NA1":  "https://na1.api.riotgames.com",
+		"OC1":  "https://oc1.api.riotgames.com",
+		"TR1":  "https://tr1.api.riotgames.com",
+		"RU":   "https://ru.api.riotgames.com",
+	}
+	return
+}
