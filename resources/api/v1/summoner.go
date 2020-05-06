e7de5a4339162060a71028a216d7f3bb3116668b
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-06 09:20:47 -0300

    ref: add response for league data and add fields in summoner struct for league data specific

--- resources/api/v1/summoner.go
+++ resources/api/v1/summoner.go
@@ -11,8 +11,40 @@ type SummonerResponse struct {
 	SummonerLevel int    `json:"summonerLevel,omitempty"`
 }
 
+// LeagueEntriesResponse - data league of summoner
+type LeagueEntriesResponse struct {
+	LeagueID      string        `json:"leagueId,omitempty"`
+	QueueType     string        `json:"queueType,omitempty"`
+	Tier          string        `json:"tier,omitempty"`
+	Rank          string        `json:"rank,omitempty"`
+	SummonerID    string        `json:"summonerId,omitempty"`
+	SummonerName  string        `json:"summonerName,omitempty"`
+	LeaguePoints  int           `json:"leaguePoints,omitempty"`
+	Wins          int           `json:"wins,omitempty"`
+	Losses        int           `json:"losses,omitempty"`
+	Veteran       bool          `json:"veteran,omitempty"`
+	Inactive      bool          `json:"inactive,omitempty"`
+	FreshBlood    bool          `json:"freshBlood,omitempty"`
+	HotStreak     bool          `json:"hotStreak,omitempty"`
+	MiniSeriesDTO MiniSeriesDTO `json:"miniSeries,omitempty"`
+}
+
+// MiniSeriesDTO -
+type MiniSeriesDTO struct {
+	Losses   int    `json:"losses,omitempty"`
+	Progress string `json:"progress,omitempty"`
+	Target   int    `json:"target,omitempty"`
+	Wins     int    `json:"wins,omitempty"`
+}
+
 // Summoner - summoner for API
 type Summoner struct {
-	Name          string `json:"name,omitempty"`
+	SummonerName  string `json:"summonerName,omitempty"`
 	SummonerLevel int    `json:"summonerLevel,omitempty"`
+	CurrentTier   string `json:"currentTier,omitempty"`
+	CurrentRank   string `json:"currentRank,omitempty"`
+	Wins          int    `json:"wins,omitempty"`
+	Losses        int    `json:"losses,omitempty"`
+	QueueType     string `json:"queueType,omitempty"`
+	LeaguePoints  int    `json:"leaguePoints,omitempty"`
 }
