babdf8457200cc126bd5a5ba6444b0670419696d
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-06 09:22:17 -0300

    ref: testing handlers

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"net/http"
+
+	handler_v1 "github.com/antonioazambuja/ionia/handlers/api/v1"
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	router := mux.NewRouter()
+	router.HandleFunc("/summoner/byname/{region}/{name}", handler_v1.GetByName).Methods("GET")
+
+	http.ListenAndServe(":5000", router)
 }
