02fd2613b0c00bebce1ea88e73185a834021a013
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-05-06 09:21:35 -0300

    ref: create function for get league data of summoner and adjust return summoner with new fields

--- services/api/v1/summoner.go
+++ services/api/v1/summoner.go
@@ -9,7 +9,7 @@ import (
 )
 
 // GetByName - get summoner by name
-func GetByName(summonerName string, regionName string) (resources_v1.SummonerResponse, error) {
+func GetByName(summonerName string, regionName string) (resources_v1.Summoner, error) {
 	regions := GetAvailableRegions()
 	request, errNewRequest := http.NewRequest("GET", regions[regionName]+"/lol/summoner/v4/summoners/by-name/"+summonerName, nil)
 	if errNewRequest != nil {
@@ -24,7 +24,44 @@ func GetByName(summonerName string, regionName string) (resources_v1.SummonerRes
 		panic(errResponse)
 	}
 	defer response.Body.Close()
-	var summoner resources_v1.SummonerResponse
-	json.NewDecoder(response.Body).Decode(&summoner)
-	return summoner, nil
+
+	var summonerResponse resources_v1.SummonerResponse
+	json.NewDecoder(response.Body).Decode(&summonerResponse)
+
+	leagueEntriesResponse, err := _GetLeagueEntries(summonerResponse.ID, regionName)
+	if err != nil {
+		panic(err)
+	}
+
+	return resources_v1.Summoner{
+		SummonerName:  summonerResponse.Name,
+		SummonerLevel: summonerResponse.SummonerLevel,
+		CurrentRank:   leagueEntriesResponse.Rank,
+		CurrentTier:   leagueEntriesResponse.Tier,
+		LeaguePoints:  leagueEntriesResponse.LeaguePoints,
+		Wins:          leagueEntriesResponse.Wins,
+		Losses:        leagueEntriesResponse.Losses,
+		QueueType:     leagueEntriesResponse.QueueType,
+	}, nil
+}
+
+// _GetLeagueEntries - get data league of summoner
+func _GetLeagueEntries(summonerID string, regionName string) (resources_v1.LeagueEntriesResponse, error) {
+	regions := GetAvailableRegions()
+	request, errNewRequest := http.NewRequest("GET", regions[regionName]+"/lol/league/v4/entries/by-summoner/"+summonerID, nil)
+	if errNewRequest != nil {
+		panic(errNewRequest)
+	}
+	request.Header.Set(HeaderAPIKey, APIKey)
+	client := &http.Client{
+		Timeout: time.Duration(300 * time.Second),
+	}
+	response, errResponse := client.Do(request)
+	if errResponse != nil && response.StatusCode != 200 {
+		panic(errResponse)
+	}
+	defer response.Body.Close()
+	var leagueEntriesResponse []resources_v1.LeagueEntriesResponse
+	json.NewDecoder(response.Body).Decode(&leagueEntriesResponse)
+	return leagueEntriesResponse[0], nil
 }
