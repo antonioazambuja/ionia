4fb2304a9875732c6d96224f24dc8e9ac25e2c42
Antonio Marcos Silva de Azambuja <antonioazambuja_@hotmail.com>
2020-06-05 19:52:06 -0300

    del: const file

--- services/api/v1/const.go
+++
@@ -1,7 +0,0 @@
-package v1
-
-// // APIKey - key of api
-// const APIKey = "RGAPI-5adac329-9c25-489f-bf96-5973da9b54a0"
-
-// // HeaderAPIKey - header name for api key
-// const HeaderAPIKey = "X-Riot-Token"
