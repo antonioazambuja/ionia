b3aafec7caede43d85a13e05b99c16447b1a8fbd
Antonio Marcos Silva de Azambuja <antonioazambuja_@hotmail.com>
2020-06-11 19:26:29 -0300

    ref: using regex check in summoner names

--- handlers/api/v1/summoner.go
+++ handlers/api/v1/summoner.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
+	v1 "github.com/antonioazambuja/ionia/resources/api/v1"
 	svc_v1 "github.com/antonioazambuja/ionia/services/api/v1"
 	"github.com/gorilla/mux"
 )
@@ -16,15 +18,20 @@ var logOperation = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshort
 func GetByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	summoner, err := svc_v1.GetByName(params["name"])
-	if err != nil {
-		logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-		logOperation.Print("Failed get summoner in service GetByName")
-		w.WriteHeader(http.StatusInternalServerError)
+	if checkSummonerName(params["name"]) {
+		summoner, err := svc_v1.GetByName(params["name"])
+		if err != nil {
+			logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+			logOperation.Print("Failed get summoner in service GetByName")
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		json.NewEncoder(w).Encode(summoner)
 	} else {
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(v1.Summoner{})
 	}
-	json.NewEncoder(w).Encode(summoner)
 	logOperation.Print("Perform GetByName")
 }
 
@@ -32,15 +39,20 @@ func GetByName(w http.ResponseWriter, r *http.Request) {
 func GetInfoByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	summoner, err := svc_v1.GetInfoByName(params["name"])
-	if err != nil {
-		logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-		logOperation.Print("Failed get summoner in service GetInfoByName")
-		w.WriteHeader(http.StatusInternalServerError)
+	if checkSummonerName(params["name"]) {
+		summoner, err := svc_v1.GetInfoByName(params["name"])
+		if err != nil {
+			logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+			logOperation.Print("Failed get summoner in service GetInfoByName")
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		json.NewEncoder(w).Encode(summoner)
 	} else {
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(v1.Summoner{})
 	}
-	json.NewEncoder(w).Encode(summoner)
 	logOperation.Print("Perform GetInfoByName")
 }
 
@@ -48,15 +60,20 @@ func GetInfoByName(w http.ResponseWriter, r *http.Request) {
 func GetMatchesByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	summoner, err := svc_v1.GetMatchesByName(params["name"])
-	if err != nil {
-		logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-		logOperation.Print("Failed get summoner in service GetMatchesByName")
-		w.WriteHeader(http.StatusInternalServerError)
+	if checkSummonerName(params["name"]) {
+		summoner, err := svc_v1.GetMatchesByName(params["name"])
+		if err != nil {
+			logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+			logOperation.Print("Failed get summoner in service GetMatchesByName")
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		json.NewEncoder(w).Encode(summoner)
 	} else {
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(v1.Summoner{})
 	}
-	json.NewEncoder(w).Encode(summoner)
 	logOperation.Print("Perform GetMatchesByName")
 }
 
@@ -64,13 +81,28 @@ func GetMatchesByName(w http.ResponseWriter, r *http.Request) {
 func GetLeagueByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	summoner, err := svc_v1.GetLeagueByName(params["name"])
-	if err != nil {
-		logOperation.Print("Failed get summoner in service GetLeagueByName")
-		w.WriteHeader(http.StatusInternalServerError)
+	if checkSummonerName(params["name"]) {
+		summoner, err := svc_v1.GetLeagueByName(params["name"])
+		if err != nil {
+			logOperation.Print("Failed get summoner in service GetLeagueByName")
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		json.NewEncoder(w).Encode(summoner)
 	} else {
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(v1.Summoner{})
 	}
 	logOperation.Print("Perform GetLeagueByName")
-	json.NewEncoder(w).Encode(summoner)
+}
+
+func checkSummonerName(summonerName string) bool {
+	checkSummonerName, errEspecialCharacters := regexp.MatchString("[$&+,:;=?@#|'<>.^*()%!-]", summonerName)
+	if errEspecialCharacters != nil {
+		logOperation := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+		logOperation.Print("Error validate summoner name - " + summonerName)
+		return false
+	}
+	return !checkSummonerName
 }
