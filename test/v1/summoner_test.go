ed827b24bfad499b106317db9b9009d34a18a312
Antonio Marcos Silva de Azambuja <antonioazambuja_@hotmail.com>
2020-06-14 01:20:58 -0300

    ref: add tests for functions get informations about summoners

--- test/v1/summoner_test.go
+++ test/v1/summoner_test.go
@@ -9,10 +9,45 @@ import (
 
 func TestGetSummonerByName(test *testing.T) {
 	test.Parallel()
-	summoner1, errSummoner1 := svc_v1.GetByName("IsBlackPanther")
-	assert.Empty(test, errSummoner1)
-	assert.Equal(test, "IsBlackPanther", summoner1.SummonerName)
-	assert.NotEmpty(test, summoner1.AccountID)
-	assert.NotEmpty(test, summoner1.LeagueInfo)
-	assert.NotEmpty(test, summoner1.MatchesInfo)
+	summoner, errSummoner := svc_v1.GetByName("IsBlackPanther")
+	assert.Empty(test, errSummoner)
+	assert.Equal(test, "IsBlackPanther", summoner.SummonerName)
+	assert.NotEmpty(test, summoner.AccountID)
+	assert.NotEmpty(test, summoner.LeagueInfo)
+	assert.NotEmpty(test, summoner.MatchesInfo)
+}
+
+func TestGetSummonerByNameInvalidSummonerName(test *testing.T) {
+	test.Parallel()
+	summoner2, errSummoner2 := svc_v1.GetByName("&*|ª")
+	assert.NotEmpty(test, errSummoner2)
+	assert.Empty(test, summoner2)
+}
+
+func TestGetSummonerInfoByName(test *testing.T) {
+	test.Parallel()
+	summoner, errSummoner := svc_v1.GetInfoByName("IsBlackPanther")
+	assert.Empty(test, errSummoner)
+	assert.Equal(test, "IsBlackPanther", summoner.SummonerName)
+	assert.NotEmpty(test, summoner.AccountID)
+	assert.Empty(test, summoner.LeagueInfo)
+	assert.Empty(test, summoner.MatchesInfo)
+}
+
+func TestGetSummonerLeagueByName(test *testing.T) {
+	test.Parallel()
+	summoner, errSummoner := svc_v1.GetLeagueByName("IsBlackPanther")
+	assert.Empty(test, errSummoner)
+	assert.Empty(test, summoner.AccountID)
+	assert.NotEmpty(test, summoner.LeagueInfo)
+	assert.Empty(test, summoner.MatchesInfo)
+}
+
+func TestGetSummonerMatchesByName(test *testing.T) {
+	test.Parallel()
+	summoner, errSummoner := svc_v1.GetMatchesByName("IsBlackPanther")
+	assert.Empty(test, errSummoner)
+	assert.Empty(test, summoner.AccountID)
+	assert.Empty(test, summoner.LeagueInfo)
+	assert.NotEmpty(test, summoner.MatchesInfo)
 }
