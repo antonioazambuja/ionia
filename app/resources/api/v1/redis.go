c00892eff4fc399492e652b764f5185098ccf8f9
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-06-23 21:34:27 -0300

    add: redis file with implementation for redis client

---
+++ app/resources/api/v1/redis.go
@@ -0,0 +1,38 @@
+package v1
+
+import (
+	"context"
+	"os"
+
+	"github.com/antonioazambuja/ionia/utils"
+
+	"github.com/go-redis/redis"
+)
+
+// RedisClientMaster - global variable of connection with Redis
+var RedisClientMaster *redis.Client
+
+// CreateRedisConnection - create new connection with Redis
+func CreateRedisConnection() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_MASTER_URL") + ":6379",
+		Password: os.Getenv("REDIS_MASTER_PWD"),
+		DB: 0,
+	})
+	pingResult, errPingResult := client.Ping(context.Background()).Result()
+	if errPingResult != nil {
+		utils.LogOperation.Println("Error found pingResultError: Failed connect in Redis host: " + os.Getenv("REDIS_MASTER_URL") + ".")
+		panic(errPingResult)
+	} else {
+		utils.LogOperation.Println("Connected in Redis: " + pingResult)
+	}
+	RedisClientMaster = client
+}
+
+// GetConn - test connection with Redis, if failed create new connection
+func GetConn() *redis.Client {
+	if pingResult := RedisClientMaster.Ping(context.TODO()); pingResult.Err() != nil {
+		CreateRedisConnection()
+	}
+	return RedisClientMaster
+}
