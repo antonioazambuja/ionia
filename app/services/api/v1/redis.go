e86dd3036afc49da2e86576c3940fed62295df43
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-04 22:32:07 -0300

    add: redis implementation service

---
+++ app/services/api/v1/redis.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	rsc_v1 "github.com/antonioazambuja/ionia/app/resources/api/v1"
+	utils "github.com/antonioazambuja/ionia/utils"
+)
+
+// RedisClientFunc - interface with functions of RedisClient
+type RedisClientFunc interface {
+	SaveSummoner(summoner *rsc_v1.Summoner, informationID string) error
+	SearchSummoner(summonerID string, informationID string) (*rsc_v1.Summoner, error)
+}
+
+// GetRedisSummoner - Service complex info summoner by name
+func GetRedisSummoner(redisClient RedisClientFunc, summonerName, informationID string) (*rsc_v1.Summoner, error) {
+	summonerCacheRedis, errSummonerCacheRedis := redisClient.SearchSummoner(summonerName, informationID)
+	if errSummonerCacheRedis != nil {
+		return nil, errSummonerCacheRedis
+	}
+	return summonerCacheRedis, nil
+}
+
+// SaveRedisSummoner - Service info summoner by name
+func SaveRedisSummoner(redisClient RedisClientFunc, summoner *rsc_v1.Summoner, informationID string) error {
+	if errNewCacheSummoner := redisClient.SaveSummoner(summoner, informationID); errNewCacheSummoner != nil {
+		utils.LogOperation.Print("Error found! Failed service GetByName: errNewCacheSummoner")
+		utils.LogOperation.Print(errNewCacheSummoner.Error())
+		return errNewCacheSummoner
+	}
+	return nil
+}
