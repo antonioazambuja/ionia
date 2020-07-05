97be54718763a8b5833b320f4571ac234b66bd4b
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-04 22:49:37 -0300

    ref: using new clients, redis and riot api

--- app/services/api/v1/summoner.go
+++ app/services/api/v1/summoner.go
@@ -7,11 +7,11 @@ import (
 	utils "github.com/antonioazambuja/ionia/utils"
 )
 
-// RiotAPIClientFunc -
+// RiotAPIClientFunc - interface with functions of RiotAPIClient
 type RiotAPIClientFunc interface {
-	GetSummonerByName(summonerName string) *rsc_v1.SummonerDTO
-	GetSummonerLeaguesByID(summonerID string) []rsc_v1.LeagueEntryDTO
-	GetSummonerMatchesByAccountID(accountID string) *rsc_v1.MatchlistDto
+	GetSummonerByName(summonerName string) (*rsc_v1.SummonerDTO, error)
+	GetSummonerLeaguesByID(summonerID string) ([]rsc_v1.LeagueEntryDTO, error)
+	GetSummonerMatchesByAccountID(accountID string) (*rsc_v1.MatchlistDto, error)
 }
 
 // SummonerV4 -
@@ -26,88 +26,80 @@ const MatchesV4 string = "/lol/match/v4/matchlists/by-account/"
 // GetByName - Service complex info summoner by name
 func GetByName(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
 	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
-		utils.LogOperation.Println("Not validate summoner name")
+		utils.LogOperation.Println(errCheckSummonerName)
 		return nil, errCheckSummonerName
 	}
-	// summonerCacheRedis, errSummonerCacheRedis := rsc_v1.GetCacheSummoner(summonerName, "full")
-	// if errSummonerCacheRedis == nil {
-	// 	return &summonerCacheRedis, nil
-	// }
-	summonerDTO := riotAPIClient.GetSummonerByName(summonerName)
-	leagueEntryDTO := riotAPIClient.GetSummonerLeaguesByID(summonerDTO.ID)
-	matchlistDto := riotAPIClient.GetSummonerMatchesByAccountID(summonerDTO.AccountID)
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return nil, errSummonerDTO
+	}
+	leagueEntryDTO, errLeagueEntryDTO := riotAPIClient.GetSummonerLeaguesByID(summonerDTO.ID)
+	if errLeagueEntryDTO != nil {
+		return nil, errLeagueEntryDTO
+	}
+	matchlistDTO, errMatchlistDTO := riotAPIClient.GetSummonerMatchesByAccountID(summonerDTO.AccountID)
+	if errMatchlistDTO != nil {
+		return nil, errMatchlistDTO
+	}
 	summoner := new(rsc_v1.Summoner)
 	summoner.WithSummonerInfo(summonerDTO)
 	summoner.WithLeagueInfo(leagueEntryDTO)
-	summoner.WithMatchesInfo(matchlistDto)
-	// if errNewCacheSummoner := rsc_v1.NewCacheSummoner(summoner, "full"); errNewCacheSummoner != nil {
-	// 	utils.LogOperation.Print("Error found! Failed service GetByName: errNewCacheSummoner")
-	// 	utils.LogOperation.Print(errNewCacheSummoner.Error())
-	// }
+	summoner.WithMatchesInfo(matchlistDTO)
 	return summoner, nil
 }
 
 // GetInfoByName - Service info summoner by name
 func GetInfoByName(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
 	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
-		utils.LogOperation.Println(errCheckSummonerName.Error())
+		utils.LogOperation.Println(errCheckSummonerName)
 		return nil, errCheckSummonerName
 	}
-	// summonerCacheRedis, errSummonerCacheRedis := rsc_v1.GetCacheSummoner(summonerName, "info")
-	// if errSummonerCacheRedis == nil {
-	// 	return &summonerCacheRedis, nil
-	// }
-	summonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return nil, errSummonerDTO
+	}
 	summoner := new(rsc_v1.Summoner)
 	summoner.WithSummonerInfo(summonerDTO)
-	// if errNewCacheSummoner := rsc_v1.NewCacheSummoner(summoner, "info"); errNewCacheSummoner != nil {
-	// 	utils.LogOperation.Print("Error found! Failed service GetInfoByName: errNewCacheSummoner")
-	// 	utils.LogOperation.Print(errNewCacheSummoner.Error())
-	// }
 	return summoner, nil
 }
 
 // GetLeagueByName - Service league info summoner by name
 func GetLeagueByName(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
 	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
-		utils.LogOperation.Println("Not validate summoner name")
+		utils.LogOperation.Println(errCheckSummonerName)
 		return nil, errCheckSummonerName
 	}
-	// summonerCacheRedis, errSummonerCacheRedis := rsc_v1.GetCacheSummoner(summonerName, "league")
-	// if errSummonerCacheRedis == nil {
-	// 	return &summonerCacheRedis, nil
-	// }
-	summonerDTO := riotAPIClient.GetSummonerByName(summonerName)
-	leagueEntryDTO := riotAPIClient.GetSummonerLeaguesByID(summonerDTO.ID)
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return nil, errSummonerDTO
+	}
+	leagueEntryDTO, errLeagueEntryDTO := riotAPIClient.GetSummonerLeaguesByID(summonerDTO.ID)
+	if errLeagueEntryDTO != nil {
+		return nil, errLeagueEntryDTO
+	}
 	summoner := new(rsc_v1.Summoner)
 	summoner.WithSummonerInfo(summonerDTO)
 	summoner.WithLeagueInfo(leagueEntryDTO)
-	// if errNewCacheSummoner := rsc_v1.NewCacheSummoner(summoner, "league"); errNewCacheSummoner != nil {
-	// 	utils.LogOperation.Print("Error found! Failed service GetLeagueByName: errNewCacheSummoner")
-	// 	utils.LogOperation.Print(errNewCacheSummoner.Error())
-	// }
 	return summoner, nil
 }
 
 // GetMatchesByName - Service matches info summoner by name
 func GetMatchesByName(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
 	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
-		utils.LogOperation.Println("Not validate summoner name")
+		utils.LogOperation.Println(errCheckSummonerName)
 		return nil, errCheckSummonerName
 	}
-	// summonerCacheRedis, errSummonerCacheRedis := rsc_v1.GetCacheSummoner(summonerName, "matches")
-	// if errSummonerCacheRedis == nil {
-	// 	return &summonerCacheRedis, nil
-	// }
-	summonerDTO := riotAPIClient.GetSummonerByName(summonerName)
-	matchlistDto := riotAPIClient.GetSummonerMatchesByAccountID(summonerDTO.AccountID)
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return nil, errSummonerDTO
+	}
+	matchlistDTO, errMatchlistDTO := riotAPIClient.GetSummonerMatchesByAccountID(summonerDTO.AccountID)
+	if errMatchlistDTO != nil {
+		return nil, errMatchlistDTO
+	}
 	summoner := new(rsc_v1.Summoner)
 	summoner.WithSummonerInfo(summonerDTO)
-	summoner.WithMatchesInfo(matchlistDto)
-	// if errNewCacheSummoner := rsc_v1.NewCacheSummoner(summoner, "full"); errNewCacheSummoner != nil {
-	// 	utils.LogOperation.Print("Error found! Failed service GetByName: errNewCacheSummoner")
-	// 	utils.LogOperation.Print(errNewCacheSummoner.Error())
-	// }
+	summoner.WithMatchesInfo(matchlistDTO)
 	return summoner, nil
 }
 
