bb8946de03f707a1ec1157a684070252e88116f1
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-04 22:44:47 -0300

    ref: using new clients for redis and riot api

--- app/handlers/api/v1/summoner.go
+++ app/handlers/api/v1/summoner.go
@@ -11,72 +11,105 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// GetByName - get summoner by name
+// GetByName - Get summoner by name
 func GetByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var statusCode int
+	statusCode := 200
+	summoner := new(rsc_v1.Summoner)
 	params := mux.Vars(r)
-	riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
-	summoner, errGetByName := svc_v1.GetByName(riotAPIClient, params["name"])
-	if errGetByName != nil {
-		utils.LogOperation.Print(errGetByName)
-		statusCode = http.StatusInternalServerError
-	} else {
-		statusCode = http.StatusOK
+	summoner, erroSummonerCached := svc_v1.GetRedisSummoner(rsc_v1.RedisClientConnected, params["name"], "full")
+	if erroSummonerCached != nil {
+		utils.LogOperation.Println(erroSummonerCached)
+		riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
+		summonerRiotAPI, errGetByName := svc_v1.GetByName(riotAPIClient, params["name"])
+		if errGetByName != nil {
+			utils.LogOperation.Print(errGetByName)
+			statusCode = http.StatusInternalServerError
+		} else {
+			statusCode = http.StatusOK
+		}
+		summoner = summonerRiotAPI
+		if errSaveCacheSummoner := svc_v1.SaveRedisSummoner(rsc_v1.RedisClientConnected, summoner, "full"); errSaveCacheSummoner != nil {
+			utils.LogOperation.Print(errSaveCacheSummoner)
+		}
 	}
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(summoner)
 	utils.ServiceLog(statusCode, r, "GetByName")
 }
 
-// GetInfoByName - get info summoner by name
+// GetInfoByName - Get info summoner by name
 func GetInfoByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var statusCode int
+	statusCode := 200
 	params := mux.Vars(r)
-	riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
-	summoner, errGetInfoByName := svc_v1.GetInfoByName(riotAPIClient, params["name"])
-	if errGetInfoByName != nil {
-		utils.LogOperation.Print(errGetInfoByName)
-		statusCode = http.StatusInternalServerError
-	} else {
-		statusCode = http.StatusOK
+	summoner, erroSummonerCached := svc_v1.GetRedisSummoner(rsc_v1.RedisClientConnected, params["name"], "info")
+	if erroSummonerCached != nil {
+		utils.LogOperation.Println(erroSummonerCached)
+		riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
+		summonerRiotAPI, errGetByName := svc_v1.GetInfoByName(riotAPIClient, params["name"])
+		if errGetByName != nil {
+			utils.LogOperation.Print(errGetByName)
+			statusCode = http.StatusInternalServerError
+		} else {
+			statusCode = http.StatusOK
+		}
+		summoner = summonerRiotAPI
+		if errSaveCacheSummoner := svc_v1.SaveRedisSummoner(rsc_v1.RedisClientConnected, summoner, "info"); errSaveCacheSummoner != nil {
+			utils.LogOperation.Print(errSaveCacheSummoner)
+		}
 	}
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(summoner)
 	utils.ServiceLog(statusCode, r, "GetInfoByName")
 }
 
-// GetMatchesByName - get info summoner by name
+// GetMatchesByName - Get info summoner by name
 func GetMatchesByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var statusCode int
+	statusCode := 200
 	params := mux.Vars(r)
-	riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
-	summoner, errGetMatchesByName := svc_v1.GetMatchesByName(riotAPIClient, params["name"])
-	if errGetMatchesByName != nil {
-		utils.LogOperation.Print(errGetMatchesByName)
-		statusCode = http.StatusInternalServerError
-	} else {
-		statusCode = http.StatusOK
+	summoner, erroSummonerCached := svc_v1.GetRedisSummoner(rsc_v1.RedisClientConnected, params["name"], "matches")
+	if erroSummonerCached != nil {
+		utils.LogOperation.Println(erroSummonerCached)
+		riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
+		summonerRiotAPI, errGetByName := svc_v1.GetMatchesByName(riotAPIClient, params["name"])
+		if errGetByName != nil {
+			utils.LogOperation.Print(errGetByName)
+			statusCode = http.StatusInternalServerError
+		} else {
+			statusCode = http.StatusOK
+		}
+		summoner = summonerRiotAPI
+		if errSaveCacheSummoner := svc_v1.SaveRedisSummoner(rsc_v1.RedisClientConnected, summoner, "matches"); errSaveCacheSummoner != nil {
+			utils.LogOperation.Print(errSaveCacheSummoner)
+		}
 	}
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(summoner)
 	utils.ServiceLog(statusCode, r, "GetMatchesByName")
 }
 
-// GetLeagueByName - get info summoner by name
+// GetLeagueByName - Get info summoner by name
 func GetLeagueByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var statusCode int
+	statusCode := 200
 	params := mux.Vars(r)
-	riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
-	summoner, errGetLeagueByName := svc_v1.GetLeagueByName(riotAPIClient, params["name"])
-	if errGetLeagueByName != nil {
-		utils.LogOperation.Print(errGetLeagueByName)
-		statusCode = http.StatusInternalServerError
-	} else {
-		statusCode = http.StatusOK
+	summoner, erroSummonerCached := svc_v1.GetRedisSummoner(rsc_v1.RedisClientConnected, params["name"], "leagues")
+	if erroSummonerCached != nil {
+		utils.LogOperation.Println(erroSummonerCached)
+		riotAPIClient := rsc_v1.NewRiotAPIClient(os.Getenv("ENDPOINT_REGION"), os.Getenv("API_KEY"), os.Getenv("HEADER_API_KEY"))
+		summonerRiotAPI, errGetByName := svc_v1.GetLeagueByName(riotAPIClient, params["name"])
+		if errGetByName != nil {
+			utils.LogOperation.Print(errGetByName)
+			statusCode = http.StatusInternalServerError
+		} else {
+			statusCode = http.StatusOK
+		}
+		summoner = summonerRiotAPI
+		if errSaveCacheSummoner := svc_v1.SaveRedisSummoner(rsc_v1.RedisClientConnected, summoner, "leagues"); errSaveCacheSummoner != nil {
+			utils.LogOperation.Print(errSaveCacheSummoner)
+		}
 	}
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(summoner)
