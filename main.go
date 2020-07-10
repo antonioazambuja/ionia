377a4dcb844faa92ba96a6d1aa1f0d52ae6823a5
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-09 22:49:20 -0300

    ref: using v1 endpoint

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func init() {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/summoner/byname/{name}", handler_v1.GetByName).Methods("GET")
-	router.HandleFunc("/summoner/byname/{name}/matches", handler_v1.GetMatchesByName).Methods("GET")
-	router.HandleFunc("/summoner/byname/{name}/league", handler_v1.GetLeagueByName).Methods("GET")
-	router.HandleFunc("/summoner/byname/{name}/info", handler_v1.GetInfoByName).Methods("GET")
-	router.HandleFunc("/health", handler_v1.HealthCheck).Methods("GET")
+	router.HandleFunc("/v1/summoner/byname/{name}", handler_v1.GetByName).Methods("GET")
+	router.HandleFunc("/v1/summoner/byname/{name}/matches", handler_v1.GetMatchesByName).Methods("GET")
+	router.HandleFunc("/v1/summoner/byname/{name}/league", handler_v1.GetLeagueByName).Methods("GET")
+	router.HandleFunc("/v1/summoner/byname/{name}/info", handler_v1.GetInfoByName).Methods("GET")
+	router.HandleFunc("/v1/health", handler_v1.HealthCheck).Methods("GET")
 	utils.LogOperation.Print("Custom Golang API Riot!")
 	utils.LogOperation.Print("Serving at port 5000!")
 	log.Fatal(http.ListenAndServe(":5000", router))
