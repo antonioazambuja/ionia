e1255e1c8fd078e44e4925b686ee1ae2ff028dc9
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-17 19:10:01 -0300

    ref: rename file

--- app/services/api/v1/riot.go
+++ app/services/api/v1/riot.go
@@ -116,3 +116,63 @@ func CheckSummonerName(summonerName string) error {
 	}
 	return nil
 }
+
+// GetMatchesCurrentYear - Service complex info summoner by name
+func GetMatchesCurrentYear(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
+	summoner := rsc_v1.NewSummoner()
+	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
+		utils.LogOperation.Println(errCheckSummonerName)
+		return &rsc_v1.Summoner{}, errCheckSummonerName
+	}
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return &rsc_v1.Summoner{}, errSummonerDTO
+	}
+	summoner.WithSummonerInfo(summonerDTO)
+	matchlistDTO, errMatchlistDTO := riotAPIClient.GetMatchesCurrentYear(summonerDTO.AccountID)
+	if errMatchlistDTO != nil {
+		return summoner, errMatchlistDTO
+	}
+	summoner.WithMatchesInfo(matchlistDTO)
+	return summoner, nil
+}
+
+// GetMatchesCurrentMonth - Service complex info summoner by name
+func GetMatchesCurrentMonth(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
+	summoner := rsc_v1.NewSummoner()
+	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
+		utils.LogOperation.Println(errCheckSummonerName)
+		return &rsc_v1.Summoner{}, errCheckSummonerName
+	}
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return &rsc_v1.Summoner{}, errSummonerDTO
+	}
+	summoner.WithSummonerInfo(summonerDTO)
+	matchlistDTO, errMatchlistDTO := riotAPIClient.GetMatchesCurrentMonth(summonerDTO.AccountID)
+	if errMatchlistDTO != nil {
+		return summoner, errMatchlistDTO
+	}
+	summoner.WithMatchesInfo(matchlistDTO)
+	return summoner, nil
+}
+
+// GetMatchesCurrentDay - Service complex info summoner by name
+func GetMatchesCurrentDay(riotAPIClient RiotAPIClientFunc, summonerName string) (*rsc_v1.Summoner, error) {
+	summoner := rsc_v1.NewSummoner()
+	if errCheckSummonerName := CheckSummonerName(summonerName); errCheckSummonerName != nil {
+		utils.LogOperation.Println(errCheckSummonerName)
+		return &rsc_v1.Summoner{}, errCheckSummonerName
+	}
+	summonerDTO, errSummonerDTO := riotAPIClient.GetSummonerByName(summonerName)
+	if errSummonerDTO != nil {
+		return &rsc_v1.Summoner{}, errSummonerDTO
+	}
+	summoner.WithSummonerInfo(summonerDTO)
+	matchlistDTO, errMatchlistDTO := riotAPIClient.GetMatchesCurrentDay(summonerDTO.AccountID)
+	if errMatchlistDTO != nil {
+		return summoner, errMatchlistDTO
+	}
+	summoner.WithMatchesInfo(matchlistDTO)
+	return summoner, nil
+}
