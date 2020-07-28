1fa33f434244eb18cb9fdf132e19c1950f151757
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-28 20:03:47 -0300

    ref: upgrade health check, checking connection with current riot api this deploy

--- app/handlers/api/v1/health.go
+++ app/handlers/api/v1/health.go
@@ -2,7 +2,23 @@ package v1
 
 import (
 	"net/http"
+	"os"
+	"time"
+
+	utils "github.com/antonioazambuja/ionia/utils"
 )
 
 // HealthCheck - get summoner by name
-func HealthCheck(w http.ResponseWriter, r *http.Request) {}
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	clientHealthCheck := &http.Client{
+		Timeout: time.Duration(300 * time.Second),
+	}
+	_, errRequestHealthCheckRiotAPI := clientHealthCheck.Get(os.Getenv("ENDPOINT_REGION"))
+	if errRequestHealthCheckRiotAPI != nil {
+		utils.LogOperation.Println(errRequestHealthCheckRiotAPI.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+}
